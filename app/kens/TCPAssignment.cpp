6a95f2122d3b49a1e4f5fd4d7c171d7c68f76000
gogogowowowow <whgustjr302@kaist.ac.kr>
2021-10-11 19:15:59 +0900

    3-way pass

--- app/kens/TCPAssignment.cpp
+++ app/kens/TCPAssignment.cpp
@@ -36,6 +36,7 @@ map<pid_fd , address_port > dest_m;
 //map<pid_fd , max_socket > listen_m;
 map<address_port , listen_que > accepted_que;
 map<address_port , int > for_listen;
+map<address_port , int > flag_listen;
 
 
 
@@ -58,9 +59,11 @@ void TCPAssignment::syscall_socket(UUID syscallUUID, int pid, int param1, int pa
 }
 void TCPAssignment::syscall_close(UUID syscallUUID, int pid, int param1)
 {
+  //printf("clos uuid is %ld\n",syscallUUID);
   pid_fd pf1=make_pair(pid,param1);
-  //accepted_que.erase(src_m[pf1]); //FIN에서 진행
-  //for_listen.erase(src_m[pf1]);   //FIN에서 진행 ?
+  accepted_que.erase(src_m[pf1]); //FIN에서 진행
+  //for_listen.erase(src_m[pf1]);   //FIN에서 진행 ? 적절한 곳에서 해야됨 
+  flag_listen.erase(src_m[pf1]);
   src_m.erase(pf1); 
   dest_m.erase(pf1); //added
   removeFileDescriptor(pid,param1);
@@ -202,9 +205,11 @@ void TCPAssignment::syscall_listen(UUID syscallUUID,int pid, int param1,
   ready_listen_q rlq;
   complete_listen_q clq;
   listen_que lq = make_pair(rlq,clq);
+  //accepted_que.erase(address_port1);
   accepted_que.insert(pair<address_port,listen_que>(address_port1, lq));
+  for_listen.erase(address_port1);
   for_listen.insert(pair<address_port, int >(address_port1, param2));
-
+  flag_listen.insert(pair<address_port, int >(address_port1, 0));
   printf("listen is %d\n",param2);
   return returnSystemCall(syscallUUID, 0);
 }
@@ -215,6 +220,7 @@ void TCPAssignment::syscall_accept(UUID syscallUUID,int pid, int param1,
   pid_fd server_pf=make_pair(pid,param1);
   
   if (src_m.find(server_pf) == src_m.end()) {
+      printf("src_m not find!\n");
      return returnSystemCall(syscallUUID, -1); 
   }
   
@@ -223,17 +229,48 @@ void TCPAssignment::syscall_accept(UUID syscallUUID,int pid, int param1,
   
   //ready_listen_q rlq = accepted_que[server_address_port].first;
   //complete_listen_q clq = accepted_que[server_address_port].second;
-
+  /*
   if(accepted_que[server_address_port].second.size()==0 && accepted_que[server_address_port].first.size()==0){
     printf("wait\n");
-    //TimerModule::addTimer(syscallUUID ,10*seconds);
-    return returnSystemCall(syscallUUID, -1); 
+    vector<any> all_information;
+    all_information.push_back(syscallUUID);
+    all_information.push_back(pid);
+    all_information.push_back(param1);
+    all_information.push_back(param2_ptr);
+    all_information.push_back(param3_ptr);
+    TimerModule::addTimer(all_information ,1*seconds);
+    //return returnSystemCall(syscallUUID, -1); 
   }
   else if(accepted_que[server_address_port].second.size()!=0){
     accepted_que[server_address_port].second.pop_front();
   }
   else{
     accepted_que[server_address_port].first.pop_front();
+  }*/
+  int accept_second=0;
+  if(accepted_que[server_address_port].second.size()!=0){
+    accepted_que[server_address_port].second.pop_front();
+  }
+  else if(accepted_que[server_address_port].first.size()!=0){
+    accepted_que[server_address_port].first.pop_front();
+  }
+  
+  else{
+    if(flag_listen[server_address_port]==0){
+      vector<any> all_information;
+      all_information.push_back(0);
+      all_information.push_back(syscallUUID);
+      all_information.push_back(pid);
+      all_information.push_back(param1);
+      all_information.push_back(param2_ptr);
+      all_information.push_back(param3_ptr);
+      TimerModule::addTimer(all_information ,0.5*seconds);
+      return;
+      accepted_que[server_address_port].first.pop_front();
+    }
+    else {
+      return returnSystemCall(syscallUUID, -1);
+    }
   }
 
   int sock_fd;
@@ -391,25 +428,28 @@ void TCPAssignment::packetArrived(std::string fromModule, Packet &&packet) {
   //printf("flag is %x \n",real_flag);
   switch(real_flag){
     case 0b00000010:{ //connect, syn
-        address_port server_address_port = make_pair(htonl(INADDR_ANY),dest_port); 
-        //ready_listen_q rlq = accepted_que[server_address_port].first;
-        //complete_listen_q clq = accepted_que[server_address_port].second;
-        /* 
-        if(for_listen[server_address_port].first!=for_listen[server_address_port].second){
-          
-          for_listen[server_address_port].second+=1;
-          //return;
+        address_port server_address_port;
+        address_port INADDR_address_port = make_pair(htonl(INADDR_ANY),dest_port);
+        address_port dest_address_port = make_pair(dest_ip,dest_port);
+        for (auto iter = src_m.begin() ; iter != src_m.end(); iter++) {
+          if(iter->second.first==INADDR_address_port.first && iter->second.second==INADDR_address_port.second){
+            server_address_port = INADDR_address_port; 
+          }
+          else{
+            server_address_port = dest_address_port; 
+          }
         }
-        accepted_que[server_address_port]+=1;
-        */
+        flag_listen[server_address_port]=1; //connect 된게 있음
         new_flag = htons(0x5012);
         new_seq_num = std::rand();//
         pkt.writeData(tcp_start+4, (uint8_t *)&new_seq_num, 4); //seq_num
         pkt.writeData(tcp_start+12, (uint8_t *)&new_flag, 2); //flag 
-        
+        //cout << "listen size is " << for_listen[server_address_port] << endl;
         if(for_listen[server_address_port]==accepted_que[server_address_port].first.size()){
-          return;
-          //break;
+          return; //=> 아예 안보냄, 아래꺼는 no ack no syn
+          new_flag = htons(0x5014);
+          pkt.writeData(tcp_start+12, (uint8_t *)&new_flag, 2);
+          break;
         }
         else{
           accepted_que[server_address_port].first.push_back(src_port);
@@ -423,7 +463,17 @@ void TCPAssignment::packetArrived(std::string fromModule, Packet &&packet) {
         pkt.writeData(tcp_start+12, (uint8_t *)&new_flag, 2); //flag
       break;
     case 0b00010000:{  //ACK
-        address_port server_address_port = make_pair(htonl(INADDR_ANY),dest_port); 
+        address_port server_address_port;
+        address_port INADDR_address_port = make_pair(htonl(INADDR_ANY),dest_port);
+        address_port dest_address_port = make_pair(dest_ip,dest_port);
+        for (auto iter = src_m.begin() ; iter != src_m.end(); iter++) {
+          if(iter->second.first==INADDR_address_port.first && iter->second.second==INADDR_address_port.second){
+            server_address_port = INADDR_address_port; 
+          }
+          else{
+            server_address_port = dest_address_port; 
+          }
+        }
         if(accepted_que[server_address_port].first.size()!=0){
           for(auto iter = accepted_que[server_address_port].first.begin(); iter!= accepted_que[server_address_port].first.end(); iter++){
             if (*iter==src_port){
@@ -462,21 +512,51 @@ void TCPAssignment::packetArrived(std::string fromModule, Packet &&packet) {
 
   uint8_t temp[20];
   pkt.readData(tcp_start, &temp, 20);
-  //new_checksum = NetworkUtil::tcp_sum(dest_ip,src_ip,temp,20); //ntoh?
-  new_checksum = NetworkUtil::tcp_sum(src_ip,dest_ip,temp,20); //ntoh?
+  
+  //new_checksum = NetworkUtil::tcp_sum(src_ip,dest_ip,temp,20); //ntoh?
+  new_checksum = NetworkUtil::tcp_sum(dest_ip,src_ip,temp,20); //ntoh?
   new_checksum = ~new_checksum;
   new_checksum = htons(new_checksum);
-  //std::cout << std::hex << new_checksum << " new_checksum " << std::endl;
   pkt.writeData(tcp_start + 16, (uint8_t *)&new_checksum, 2);
   num_packet+=1;
   sendPacket("IPv4", std::move(pkt));
   
+  if (real_flag==0b00010001){
+    Packet pkt3 = pkt.clone();  // cloning pkt. pkt2 has different UUID
+    //Packet pkt3 = pkt;          // copying pkt. pkt3 has same UUID
+    new_seq_num=htonl(ntohl(new_seq_num)+1); //o
+    new_flag = htons(0x5011);
+    pkt3.writeData(tcp_start+4, (uint8_t *)&new_seq_num, 4); //seq_num
+    pkt3.writeData(tcp_start+12, (uint8_t *)&new_flag, 2); //flag
+    new_checksum = NetworkUtil::tcp_sum(dest_ip,src_ip,temp,20); //ntoh?
+    new_checksum = ~new_checksum;
+    new_checksum = htons(new_checksum);
+    pkt3.writeData(tcp_start + 16, (uint8_t *)&new_checksum, 2);
+    vector<any> all_information;
+    all_information.push_back(1);
+    all_information.push_back(pkt3);
+    sendPacket("IPv4", std::move(pkt3));
+    //TimerModule::addTimer(all_information,0.5*seconds); //1초 뒤에 보냄 정확한 시간은 나중에 고쳐야 됨
+  }
 } 
 
 void TCPAssignment::timerCallback(std::any payload) {
-  //int a =std::any_cast<int>(payload);
-  //std::cout << "a is !!!!!!!!" << a << std::endl;
-
+  
+  vector<any> all_information = any_cast<vector<any>>(payload);
+  switch (any_cast<int>(all_information[0])) {
+    case 0:
+      syscall_accept(any_cast<UUID>(all_information[1]), any_cast<int>(all_information[2])
+      , any_cast<int>(all_information[3]), any_cast<sockaddr*>(all_information[4]), any_cast<socklen_t*>(all_information[5]));
+      break;
+    case 1:{
+      sendPacket("IPv4", std::move(any_cast<Packet>(all_information[1])));
+      break;
+    }
+    default:
+      printf("error\n");
+      assert(0);
+      break;
+  }
 }
 
 
