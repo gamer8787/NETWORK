8d90b6588b0f4001661e5309cbb66288097a1c3a
root <root@DESKTOP-F7OMSI3.localdomain>
2021-10-12 02:33:08 +0900

    test 1,2 pass

--- app/kens/TCPAssignment.cpp
+++ app/kens/TCPAssignment.cpp
@@ -27,17 +27,15 @@ typedef pair<uint32_t, uint16_t>  address_port;
 typedef list<uint16_t> ready_listen_q ;
 typedef list<uint16_t> complete_listen_q ;
 typedef pair<ready_listen_q,complete_listen_q> listen_que;
-
+typedef tuple<uint32_t, uint16_t, uint32_t,uint16_t> Four_tuple;
 
 #define seconds pow(10,9)
 
 map<pid_fd , address_port > src_m;
-map<pid_fd , address_port > dest_m;
-//map<pid_fd , max_socket > listen_m;
 map<address_port , listen_que > accepted_que;
 map<address_port , int > for_listen;
 map<address_port , int > flag_listen;
-
+map<pid_fd , Four_tuple > cli_m;
 
 
 TCPAssignment::TCPAssignment(Host &host)
@@ -65,7 +63,7 @@ void TCPAssignment::syscall_close(UUID syscallUUID, int pid, int param1)
   //for_listen.erase(src_m[pf1]);   //FIN에서 진행 ? 적절한 곳에서 해야됨 
   flag_listen.erase(src_m[pf1]);
   src_m.erase(pf1); 
-  dest_m.erase(pf1); //added
+  cli_m.erase(pf1);
   removeFileDescriptor(pid,param1);
   return returnSystemCall(syscallUUID, 0); //두 번째 미정
 }
@@ -99,7 +97,7 @@ void TCPAssignment::syscall_bind(UUID syscallUUID, int pid, int param1_int, sock
 
   return returnSystemCall(syscallUUID, 0);
 }
-
+/*
 void TCPAssignment::syscall_getsockname(UUID syscallUUID, int pid, int param1,
     sockaddr * param2_ptr, socklen_t* param3_ptr){
 
@@ -118,12 +116,41 @@ void TCPAssignment::syscall_getsockname(UUID syscallUUID, int pid, int param1,
 
   return returnSystemCall(syscallUUID, 0);
 }
+*/
+void TCPAssignment::syscall_getsockname(UUID syscallUUID, int pid, int param1,
+    sockaddr * param2_ptr, socklen_t* param3_ptr){
+  pid_fd pf1=make_pair(pid,param1);
+  address_port ap1;
+  if ( (src_m.find(pf1) == src_m.end()) && (cli_m.find(pf1) == cli_m.end()) ) {
+     return returnSystemCall(syscallUUID, -1); 
+  }
+  else if ( (src_m.find(pf1) != src_m.end()) && (cli_m.find(pf1) == cli_m.end()) ) {
+     ap1 = src_m[pf1];
+  }
+  else if( (src_m.find(pf1) == src_m.end()) && (cli_m.find(pf1) != cli_m.end()) ) {
+    Four_tuple ssdd = cli_m[pf1];
+    ap1 = make_pair(get<0>(ssdd),get<1>(ssdd));
+  }
+  else{
+    ap1 = src_m[pf1];
+    //perror("didn't considerf\n");
+  }
+  struct sockaddr_in* socksock = (sockaddr_in*) param2_ptr;
+  //memset(&socksock, 0, sizeof(socksock));
+  socksock->sin_family = AF_INET;
+  socksock->sin_addr.s_addr =ap1.first;
+  socksock->sin_port =ap1.second;
+  *param3_ptr = sizeof(param2_ptr);
+
+  return returnSystemCall(syscallUUID, 0);
+}
+
 
 void TCPAssignment::syscall_connect(UUID syscallUUID, int pid, int param1, 
    sockaddr*param2_ptr,socklen_t param3){
   
   printf("in connect!!!\n");
-  /*
+  
   struct sockaddr_in* socksock = (sockaddr_in *)param2_ptr;
   ipv4_t dest_ip ;  
   for(int i=0;i<4;i++){
@@ -132,7 +159,8 @@ void TCPAssignment::syscall_connect(UUID syscallUUID, int pid, int param1,
   int port = getRoutingTable(dest_ip); 
   std::optional<ipv4_t> ip = getIPAddr(port); // retrieve the source IP address
   ipv4_t ip_real = ip.value();
-  uint32_t ip_32 = ip_real[3] | (ip_real[2] << 8) | (ip_real[1] << 16) | (ip_real[0] << 24);
+  uint32_t ip_32 = NetworkUtil::arrayToUINT64<4>(ip_real);
+  //uint32_t ip_32 = ip_real[3] | (ip_real[2] << 8) | (ip_real[1] << 16) | (ip_real[0] << 24);
 
   uint16_t port_16 = (in_port_t) port;
   //std::optional<mac_t> mac = getMACAddr(port); // retrieve the source MAC address; used only in the IP layer
@@ -150,11 +178,6 @@ void TCPAssignment::syscall_connect(UUID syscallUUID, int pid, int param1,
   uint16_t source_port = std::rand(); //있는 거 생성하면 안됨 나중에 생각
   uint16_t dest_port = socksock->sin_port;
 
-  src_ip_32 = htonl(src_ip_32);
-  dest_ip_32 = htonl(dest_ip_32);
-  source_port = htons(source_port);
-  dest_port = htons(dest_port);
-
   pkt.writeData(ip_start+12, (uint8_t *)&src_ip_32, 4);
   pkt.writeData(ip_start+16, (uint8_t *)&dest_ip_32, 4);
   pkt.writeData(tcp_start, (uint8_t *)&source_port, 2);
@@ -162,38 +185,36 @@ void TCPAssignment::syscall_connect(UUID syscallUUID, int pid, int param1,
 
   uint32_t seq_num =1;
   uint32_t ack_num =0;
-  uint16_t flag = 0b10; 
-  uint16_t urg = 0; 
+  uint16_t flag = 0x5002; 
 
   seq_num = htonl(seq_num);
   ack_num = htonl(ack_num);
   flag = htons(flag);
-  urg = htons(urg);
 
   pkt.writeData(tcp_start+4, (uint8_t *)&dest_port, 4);
   pkt.writeData(tcp_start+8, (uint8_t *)&dest_port, 4);
   pkt.writeData(tcp_start+12, (uint8_t *)&dest_port, 2);
-  pkt.writeData(tcp_start+18, (uint8_t *)&dest_port, 2);
 
   uint8_t temp[20];
   pkt.readData(tcp_start, &temp, 20);
 
-  uint16_t checksum = NetworkUtil::tcp_sum(ntohl(src_ip_32),ntohl(dest_ip_32),temp,20); //
+  uint16_t checksum = NetworkUtil::tcp_sum(src_ip_32,dest_ip_32,temp,20); //
   checksum = ~checksum;
   checksum = htons(checksum);
   pkt.writeData(tcp_start + 16, (uint8_t *)&checksum, 2);
 
   sendPacket("IPv4", std::move(pkt));
+  
 
-  address_port ap1=make_pair(ip_32,port_16);
-  address_port ap2=make_pair(ntohl(dest_ip_32),ntohs(dest_port));
+  pid_fd pf1 = make_pair(pid, param1);
+  address_port ap1 = make_pair(dest_ip_32,dest_port);
 
-  pid_fd pf1=make_pair(pid,param1);
-  src_m.insert(pair<pid_fd, address_port>(pf1, ap1));
-  dest_m.insert(pair<pid_fd, address_port>(pf1, ap2));
-  printf("in connect!!!\n");
-  */
-  return returnSystemCall(syscallUUID, 0);
+
+  Four_tuple ssdd= make_tuple(src_ip_32, source_port, dest_ip_32, dest_port);
+  cli_m.insert(pair<pid_fd, Four_tuple>(pf1, ssdd));
+  //return;
+  returnSystemCall(syscallUUID, 0);
+  
 }
 
 void TCPAssignment::syscall_listen(UUID syscallUUID,int pid, int param1, 
@@ -225,28 +246,6 @@ void TCPAssignment::syscall_accept(UUID syscallUUID,int pid, int param1,
   }
   
   address_port server_address_port = src_m[server_pf];
-  //if(for_listen[server_address_port].second==0){
-  
-  //ready_listen_q rlq = accepted_que[server_address_port].first;
-  //complete_listen_q clq = accepted_que[server_address_port].second;
-  /*
-  if(accepted_que[server_address_port].second.size()==0 && accepted_que[server_address_port].first.size()==0){
-    printf("wait\n");
-    vector<any> all_information;
-    all_information.push_back(syscallUUID);
-    all_information.push_back(pid);
-    all_information.push_back(param1);
-    all_information.push_back(param2_ptr);
-    all_information.push_back(param3_ptr);
-    TimerModule::addTimer(all_information ,1*seconds);
-    //return returnSystemCall(syscallUUID, -1); 
-  }
-  else if(accepted_que[server_address_port].second.size()!=0){
-    accepted_que[server_address_port].second.pop_front();
-  }
-  else{
-    accepted_que[server_address_port].first.pop_front();
-  }*/
   int accept_second=0;
   if(accepted_que[server_address_port].second.size()!=0){
     accepted_que[server_address_port].second.pop_front();
@@ -294,20 +293,21 @@ void TCPAssignment::syscall_accept(UUID syscallUUID,int pid, int param1,
 void TCPAssignment::syscall_getpeername(UUID syscallUUID, int pid, int param1,
     	sockaddr * param2_ptr, socklen_t*param3_ptr){
 
-  /*
+  
   pid_fd pf1=make_pair(pid,param1);
-  if (dest_m.find(pf1) == dest_m.end()) {
+  if (cli_m.find(pf1) == cli_m.end()) {
      return returnSystemCall(syscallUUID, -1); 
   }
-  
-  address_port ap1 = dest_m[pf1];
+  Four_tuple ssdd = cli_m[pf1];
+  address_port ap1 = make_pair(get<2>(ssdd),get<3>(ssdd));
   struct sockaddr_in* socksock = (sockaddr_in*) param2_ptr;
   //memset(&socksock, 0, sizeof(socksock));
   socksock->sin_family = AF_INET;
   socksock->sin_addr.s_addr =ap1.first;
   socksock->sin_port =ap1.second;
   *param3_ptr = sizeof(param2_ptr);
-  */
+  
+
   return returnSystemCall(syscallUUID, 0);
 }
 
@@ -424,7 +424,7 @@ void TCPAssignment::packetArrived(std::string fromModule, Packet &&packet) {
 
   uint16_t new_flag;
   uint16_t new_checksum;
-  //printf("flag is %x src_port is %x\n",real_flag, src_port);
+  printf("flag is %x src_port is %x\n",real_flag, src_port);
   //printf("flag is %x \n",real_flag);
   switch(real_flag){
     case 0b00000010:{ //connect, syn
@@ -446,9 +446,12 @@ void TCPAssignment::packetArrived(std::string fromModule, Packet &&packet) {
         pkt.writeData(tcp_start+12, (uint8_t *)&new_flag, 2); //flag 
         //cout << "listen size is " << for_listen[server_address_port] << endl;
         if(for_listen[server_address_port]==accepted_que[server_address_port].first.size()){
-          return; //=> 아예 안보냄, 아래꺼는 no ack no syn
-          new_flag = htons(0x5014);
-          pkt.writeData(tcp_start+12, (uint8_t *)&new_flag, 2);
+          if(for_listen[server_address_port]==0){
+            break;
+          }
+          else{
+            return; //=> 아예 안보냄, 아래꺼는 no ack no syn
+          }
           break;
         }
         else{
